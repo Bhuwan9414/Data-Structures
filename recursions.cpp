f748c733e37607c212bb96868553861a54e2c8dd
Bhuwan001 <115343236+Bhuwan9414@users.noreply.github.com>
2024-07-25 12:41:18 +0530

    Add files via upload
    In this chapter of we studied the basic concepts how how recusrion works and how to use it for optimizing the code efficiently.

---
+++ recursions.cpp
@@ -0,0 +1,77 @@
+
+// factorial problem : 
+
+// #include<iostream>
+// using namespace std;
+
+// int fact(int n){
+//     // base case
+//     // if(n == 0)
+//     // return 1;
+
+//     return n * fact(n-1);
+// }
+
+
+// int main(){
+//     int n;
+//     cout << "enter the number" << endl;
+//     cin >> n;
+
+//     int ans = fact(n);
+
+//     cout << ans << endl;
+    
+// }
+
+
+// power of 2 problem :
+
+// #include<iostream>
+// using namespace std;
+
+// int power(int n){
+//     if(n == 0)
+//     return 1;
+
+//     return 2 * power(n-1);
+// }
+
+// int main(){
+//     int n;
+//     cout << "enter the number" << endl;
+//     cin >> n;
+
+//     int ans = power(n);
+//     cout << ans << endl;
+// }
+
+
+// print counting :
+
+// #include<iostream>
+// using namespace std;
+// int count(int n){
+
+//         if(n == 0){
+//         return 1;
+//         }
+
+//         cout << n << endl;
+
+//         // recursive relation: 
+        
+//         count(n-1);
+// }
+
+// int main(){
+//     int n;
+//     cout << "enter the number" << endl;
+//     cin >> n;
+
+//     int ans = count(n);
+//     cout << ans;
+// }
+
+
+
